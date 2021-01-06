82eccf14213d728d3f946305fad6de1556a63c5f
Aleksas Pielikis <ant.kampo@gmail.com>
2021-01-06 11:00:59 +0200

    Update prepare_liepa.py

--- prepare_liepa.py
+++ prepare_liepa.py
@@ -3,13 +3,13 @@ import random
 import math
 import argparse
 
-def get_data(voice, transcript_list):
+def get_data(voice, subfolder, transcript_list):
     with open(os.path.join(voice, transcript_list), 'rt', encoding='utf-8') as fp:
         for idx, text in enumerate(fp.readlines()):
-            yield os.path.join(voice, "%d.wav" % idx), text
+            yield os.path.join(voice, subfolder, "%d.wav" % idx), text
 
-def get_subsets(proportions, voice, transcript_list):
-    data = list(get_data(voice, transcript_list))
+def get_subsets(proportions, voice, subfolder, transcript_list):
+    data = list(get_data(voice, subfolder, transcript_list))
     random.shuffle(data)
     start = 0
     for i, p in enumerate(proportions):
@@ -23,7 +23,7 @@ def get_subsets(proportions, voice, transcript_list):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Prepare liepa dataset fot training.')
     parser.add_argument('-v', '--voice', default='Regina', dest='voice', help="One of available voices [Regina, Aiste, Vladas, Edvardas]")
-    parser.add_argument('-t', '--transcript-list', default='db_tr.txt', dest='transcript_list', help="File containing lines of text recorded audio files with line number corresponding to audio file name." )
+    parser.add_argument('-t', '--transcript-list', default='db_tr.txt', dest='transcript_list', help="File containing lines of text recorded audio files with line number corresponding to audio file name.$
 
     args = parser.parse_args()
 
@@ -41,7 +41,7 @@ if __name__ == "__main__":
 
     proportions = [p for _, p in sets_metadata]
 
-    subsets = get_subsets(proportions=proportions, voice=args.voice, transcript_list=args.transcript_list)
+    subsets = get_subsets(proportions=proportions, voice=args.voice, subfolder="data", transcript_list=args.transcript_list)
 
     for path, subset in zip([p for p,_ in sets_metadata], subsets):
         with open(path, 'wt', encoding='utf-8') as fp:
